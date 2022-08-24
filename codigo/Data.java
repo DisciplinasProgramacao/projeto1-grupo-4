57f0144f544ed5108e00bdecbb9a750f1c1e5e22
Frederico-dos-Santos <fredericoandrade7@gmail.com>
2022-08-24 17:12:20 -0300

    feat: Classe data #6

---
+++ codigo/Data.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Data {
+	Scanner date = new Scanner(System.in);
+
+	private String data;
+	private int dia;
+	private int mes;
+	private int ano;
+
+	Data() {
+		this.data = null;
+		this.dia = 0;
+		this.mes = 0;
+		this.ano = 0;
+	}
+
+	public void leData() {
+		System.out.println("Digite a data desejada: ");
+		data = date.nextLine();
+	}
+
+	public boolean validaData() {
+		if (dia > 0 && dia < 32) {
+			if (mes > 0 && mes < 13) {
+				if (ano > 2021) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public void formataData() {
+		this.dia = Integer.parseInt(data.substring(0, 2));
+		this.mes = Integer.parseInt(data.substring(3, 5));
+		this.ano = Integer.parseInt(data.substring(6, 10));
+	}
+}
